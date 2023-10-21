b58eb8bcae85f7b28ac5876a0342fe9d2eac53c8
Lard Mint <lardmint@gmail.com>
2023-10-21 18:39:11 +0000

    Feat: Implement Create TimeStamp

--- src/api/v1/creatorarmour/creatorarmour.controller.ts
+++ src/api/v1/creatorarmour/creatorarmour.controller.ts
@@ -1,25 +1,56 @@
 import { Request, Response } from "express";
-import { BAD_REQUEST, OK } from "../../../resources/utils/constant.utils";
-import { getFiles } from "./creatorarmour.services";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK, ONE } from "../../../resources/utils/constant.utils";
+import { storeFiles } from "../../web3storage";
+import creatorarmourServices from "./creatorarmour.services";
+import { CreatedWork } from "./creatorarmour.schema";
 
-const getCreatedWork = async (request: Request, response: Response) => {
-    const { cid } = request.query;
-
-    if (!cid) {
-        response.status(BAD_REQUEST).send({})
+class CreatorArmourController {
+    getCreatedWork = async (request: Request, response: Response) => {
+        const { cid, chainName } = request.query;
+    
+        if (!cid) {
+            response.status(BAD_REQUEST).send({})
+        }
+    
+        let work: CreatedWork | null = null;
+    
+        try {
+            work = await creatorarmourServices.getFiles(cid as string, chainName);
+        } catch (error: any) {
+            return response.status(INTERNAL_SERVER_ERROR).
+                send({
+                    message: `An Error Ocurred: \n${error.message}`
+                });
+        }
+    
+        return response.status(OK).send({ message: "Success", data: work });
     }
-
-    try {
-
-    } catch (error: any) {
-
+    
+    createTimeStamp = async (request: Request, response: Response) => {
+        const { files, chainId } = request.body;
+    
+        if (!files || files.length < ONE || !chainId) {
+            return response.status(BAD_REQUEST).send({ message: "Invalid Parameters Sent" });
+        }
+    
+        let cid;
+        let hash;
+    
+        try {
+            cid = await storeFiles(files);
+    
+            hash = await creatorarmourServices.getTimeStampHash(cid as string, chainId);
+        } catch (error: any) {
+            return response.status(INTERNAL_SERVER_ERROR).send({ message: `An Error Ocurred: \n${error.message}` })
+        }
+    
+        return response.status(OK).send(
+            {
+                message: "Success",
+                Data: { transactionHash: hash }
+            }
+        );
     }
-
-    const work = await getFiles(cid as string);
-
-    if (!work) {
-        return
-    }
-
-    return response.status(OK).send({ message: "", data: work });
 }
+
+export default new CreatorArmourController();
